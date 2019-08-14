37f8d3bf21bb5a1336aa3bcf1afd89405d247d88
linda <1781612485@qq.com>
2019-08-14 19:13:58 +0800

    secondcommit

---
+++ src/components/uploadtest.vue
@@ -0,0 +1,45 @@
+<template>
+  <div >
+	  <!--<form enctype="multipart/form-data"method="post" action="http://localhost:8086/upload">
+	  	<input type="file" name="file"  />
+	  	<input type="submit" value="提交" />
+	  </form>-->
+	  	<input type="file" name="file"  />
+	  	<input type="submit" value="提交" v-on:click="upload"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'uploadtest',
+  methods:{
+  	upload:function(){
+  		$("[name=file]")
+  		var formdata=new window.FormData();
+  		formdata.append("file",document.querySelector("input[name=file]").files[0]);
+  	
+  		
+  		this.$http.post("http://localhost:8086/upload",formdata,{
+  			headers:{
+  				"Content-type":"multipart/form-data"
+  			}
+  		}).then(function(result){
+  			console.log(result);
+  			console.log(result.bodyText);
+  			alert("文件上传成功");
+  			this.$router.go(-1)
+  		},function(error){
+  			console.log(error.body.message);
+  		})
+  	
+  	}
+  }
+ 
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
